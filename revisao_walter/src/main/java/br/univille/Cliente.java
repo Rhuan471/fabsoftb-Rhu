94794488de170a77503e337d387751d2f709e598
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-03-17 21:54:11 -0300

    Update Cliente.java

--- revisao_walter/src/main/java/br/univille/Cliente.java
+++ revisao_walter/src/main/java/br/univille/Cliente.java
@@ -4,6 +4,18 @@ public class Cliente {
     //variável - atributo
     private String nome;
 
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+    //polimorfismo - várias formas de existir a mesma coisa
+    public Cliente() {
+
+    }
+    //método
+    public String toString() {
+        return getNome();
+    }
+
     //método
     public String getNome() {
         return nome;

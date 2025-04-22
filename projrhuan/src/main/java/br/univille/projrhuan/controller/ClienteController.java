59022a390b52b1712406ed852eb1b7e33d782c87
RHUAN JOSE VOLTOLINI <rhuanvoltolini@univille.br>
2025-04-22 20:24:32 -0300

    feat

--- projrhuan/src/main/java/br/univille/projrhuan/controller/ClienteController.java
+++ projrhuan/src/main/java/br/univille/projrhuan/controller/ClienteController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +30,7 @@ public class ClienteController {
         return new ResponseEntity<List<Cliente>>(listaClientes, HttpStatus.OK);
     }
 
-    @PostMapping 
+
     public ResponseEntity<Cliente> postCliente(@RequestBody Cliente cliente){
             if(cliente == null){
                 return ResponseEntity.badRequest().build();
@@ -39,4 +41,40 @@ public class ClienteController {
             }
             return ResponseEntity.badRequest().build();
     }
-}
+    @PutMapping("/{id}") 
+    public ResponseEntity<Cliente>  putCliente(@PathVariable long id, @RequestBody Cliente cliente){
+
+        if(id <= 0 || cliente == null){
+            return ResponseEntity.badRequest().build();
+        }
+        var clienteAntigo = service.getById(id);
+        if(clienteAntigo == null){
+            return ResponseEntity.notFound().build();
+        }
+        clienteAntigo.setNome(cliente.getNome());
+        clienteAntigo.setEndereco(cliente.getEndereco());
+        clienteAntigo.setTelefone(cliente.getTelefone());
+        clienteAntigo.setEmail(cliente.getEmail());
+        clienteAntigo.setDataNascimento(cliente.getDataNascimento());
+
+        service.save(clienteAntigo);
+        return new ResponseEntity<Cliente>(clienteAntigo,
+                HttpStatus.OK);
+    }
+
+
+	@DeleteMapping("/{id}")
+    public ResponseEntity<Cliente> deleteCliente(@PathVariable long id){
+        if(id <=0){
+            return ResponseEntity.badRequest().build();
+        }
+
+        var clienteExcluido = service.getById(id);
+        if(clienteExcluido == null){
+            return ResponseEntity.notFound().build();
+        }
+        service.delete(id);
+
+        return new ResponseEntity<Cliente>(clienteExcluido,
+                HttpStatus.OK);
+    }
--- projrhuan/src/main/java/br/univille/projrhuan/entity/Cliente.java
+++ projrhuan/src/main/java/br/univille/projrhuan/entity/Cliente.java
@@ -9,14 +9,51 @@ import jakarta.persistence.Id;
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String nome;
+    private String endereco;
+    private int getDataNascimento;
+    
+    public int getGetDataNascimento() {
+        return getDataNascimento;
+    }
 
-    public Long getId() {
+    public void setGetDataNascimento(int getDataNascimento) {
+        this.getDataNascimento = getDataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getEmail() {
+        return email;
+    }
+
+    public void setEmail(int email) {
+        this.email = email;
+    }
+
+    private int telefone;
+    private int email;
+
+    public long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -27,4 +64,5 @@ public class Cliente {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
 }

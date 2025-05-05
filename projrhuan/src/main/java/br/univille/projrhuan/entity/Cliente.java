7b733d0e7320eb5da4851d0a96cd68f9f0828403
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-05-05 14:06:39 -0300

    Update Cliente.java

--- projrhuan/src/main/java/br/univille/projrhuan/entity/Cliente.java
+++ projrhuan/src/main/java/br/univille/projrhuan/entity/Cliente.java
@@ -13,15 +13,29 @@ import jakarta.persistence.TemporalType;
 
 @Entity
 public class Cliente {
+
+    public Cliente(String joão_Silva, String string, String rua_das_Flores_123) {
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
+    private String cpf;
+    public String getCpf() {
+        return cpf;
+
+
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
     private String endereco;
     private String telefone;
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dataNascimento;
+    private Farmaceutico farmaceuticoResponsavel;
+    private OperadorCaixa operadorDeCaixaResponsavel;
     
 
     public long getId() {
@@ -56,7 +70,30 @@ public class Cliente {
         this.telefone = telefone;
     }
     
-    
-    
+    public Farmaceutico getFarmaceuticoResponsavel() {
+        return farmaceuticoResponsavel;
+    }
 
+    public void setFarmaceuticoResponsavel(Farmaceutico farmaceuticoResponsavel) {
+        this.farmaceuticoResponsavel = farmaceuticoResponsavel;
+    }
+
+    public OperadorCaixa getOperadorDeCaixaResponsavel() {
+        return operadorDeCaixaResponsavel;
+    }
+
+    public void setOperadorDeCaixaResponsavel(OperadorCaixa operadorDeCaixaResponsavel) {
+        this.operadorDeCaixaResponsavel = operadorDeCaixaResponsavel;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+            "nome='" + nome + '\'' +
+            ", cpf='" + cpf + '\'' +
+            ", endereco='" + endereco + '\'' +
+            (farmaceuticoResponsavel != null ? ", farmaceuticoResponsavel=" + farmaceuticoResponsavel.getNome() : "") +
+            (operadorDeCaixaResponsavel != null ? ", operadorDeCaixaResponsavel=" + operadorDeCaixaResponsavel.getNome() : "") +
+            '}';
+    }
 }

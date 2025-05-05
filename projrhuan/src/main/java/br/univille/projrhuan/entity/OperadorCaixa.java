fe3afd6049fb1d65fbf2cb69f7ba6ef98a4316f2
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-05-05 14:07:58 -0300

    Update OperadorCaixa.java

--- projrhuan/src/main/java/br/univille/projrhuan/entity/OperadorCaixa.java
+++ projrhuan/src/main/java/br/univille/projrhuan/entity/OperadorCaixa.java
@@ -1,22 +1,48 @@
 package br.univille.projrhuan.entity;
 
 public class OperadorCaixa {
-    private Long id;
     private String nome;
+    private String matricula;
 
-    public Long getId() {
-        return id;
+    public OperadorCaixa(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    // Getters e Setters
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void adicionarOperadorSupervisionado(Object operador) {
+        if (operador instanceof Farmaceutico || operador instanceof OperadorCaixa) {
+            // Adiciona o operador à lista de operadores supervisionados
+            // (implementação não mostrada aqui, pois não há lista definida na classe)
+        } else if (operador instanceof GerenteEstoque) {
+            // Adiciona o gerente de estoque à lista de operadores supervisionados
+            // (implementação não mostrada aqui, pois não há lista definida na classe)
+        } else {
+            throw new IllegalArgumentException("Operador deve ser Farmaceutico ou OperadorCaixa");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "OperadorDeCaixa{" +
+            "nome='" + nome + '\'' +
+            ", matricula='" + matricula + '\'' +
+            '}';
     }
 }

b57fd186af04db2ffd6b0906e441822888ee847b
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-05-05 14:07:10 -0300

    Update Farmaceutico.java

--- projrhuan/src/main/java/br/univille/projrhuan/entity/Farmaceutico.java
+++ projrhuan/src/main/java/br/univille/projrhuan/entity/Farmaceutico.java
@@ -1,22 +1,67 @@
 package br.univille.projrhuan.entity;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Farmaceutico {
-    private long id;
     private String nome;
+    private String crf;
+    
+    public class Administrador {
+        private List<Farmaceutico> farmaceuticoSupervisionados = new ArrayList<>();
+    
+        // Existing fields and methods
+    
+        public List<Farmaceutico> getFarmaceuticoSupervisionados() {
+            return farmaceuticoSupervisionados;
+        }
 
-    public long getId() {
-        return id;
+        public void setFarmaceuticoSupervisionados(List<Farmaceutico> farmaceuticoSupervisionados) {
+            this.farmaceuticoSupervisionados = farmaceuticoSupervisionados;
+        }
+
+        public void adicionarFarmaceuticoSupervisionado(Farmaceutico farmaceutico) {
+            farmaceuticoSupervisionados.add(farmaceutico);
+        }
+    
+        @Override
+        public String toString() {
+            return "Administrador{" +
+                    "farmaceuticosSupervisionados=" + farmaceuticoSupervisionados +
+                    '}';
+        }
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public Farmaceutico(String nome, String crf) {
+        this.nome = nome;
+        this.crf = crf;
     }
 
+    // Getters e Setters
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCrf() {
+        return crf;
+    }
+
+    public void setCrf(String crf) {
+        this.crf = crf;
+    }
+
+    public void adicionarFarmaceuticoSupervisionado(Farmaceutico farmaceutico) {
+        // Adiciona o farmaceutico à lista de farmaceuticos supervisionados
+        // (implementação não mostrada aqui, pois não há lista definida na classe)
+    }
+
+    @Override
+    public String toString() {
+        return "Farmaceutico{" +
+            "nome='" + nome + '\'' +
+            ", crf='" + crf + '\'' +
+            '}';
     }
 }

2c1d023504e604b31003c2c1ad5e041f2388949a
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-05-05 14:06:15 -0300

    Update Administrador.java

--- projrhuan/src/main/java/br/univille/projrhuan/entity/Administrador.java
+++ projrhuan/src/main/java/br/univille/projrhuan/entity/Administrador.java
@@ -1,20 +1,57 @@
 package br.univille.projrhuan.entity;
 
+import java.util.List;
+
 public class Administrador {
     private long id;
     private String nome;
-    private String endereco;
+    private String registro;
+    private List<OperadorCaixa> operadoresSupervisionados;
+    public List<OperadorCaixa> getOperadoresSupervisionados() {
+        return operadoresSupervisionados;
+    }
+
+
+    public void setOperadoresSupervisionados(List<OperadorCaixa> operadoresSupervisionados) {
+        this.operadoresSupervisionados = operadoresSupervisionados;
+    }
+
+
+    public List<Farmaceutico> getFarmaceuticosSupervisionados() {
+        return farmaceuticosSupervisionados;
+    }
+
+
+    public void setFarmaceuticosSupervisionados(List<Farmaceutico> farmaceuticosSupervisionados) {
+        this.farmaceuticosSupervisionados = farmaceuticosSupervisionados;
+    }
+
+
+    public GerenteEstoque getGerenteDeEstoqueSupervisionado() {
+        return gerenteDeEstoqueSupervisionado;
+    }
+
+
+    public void setGerenteDeEstoqueSupervisionado(GerenteEstoque gerenteDeEstoqueSupervisionado) {
+        this.gerenteDeEstoqueSupervisionado = gerenteDeEstoqueSupervisionado;
+    }
+
+    private List<Farmaceutico> farmaceuticosSupervisionados;
+    private GerenteEstoque gerenteDeEstoqueSupervisionado;
+    public String getRegistro() {
+        return registro;
+    }
+
+
+    public void setRegistro(String registro) {
+        this.registro = registro;
+    }
+
+
     public void setId(long id) {
         this.id = id;
     }
 
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
 
     public int getTelefone() {
         return telefone;
@@ -40,5 +77,23 @@ public class Administrador {
 
     public void setNome(String nome) {
         this.nome = nome;
+
     }
+    @Override
+    public String toString() {
+        return "Administrador{" +
+            "nome='" + nome + '\'' +
+            ", registro='" + registro + '\'' +
+            ", operadoresSupervisionados=" + operadoresSupervisionados.stream().map(OperadorCaixa::getNome).collect(java.util.stream.Collectors.toList()) +
+            ", farmaceuticosSupervisionados=" + farmaceuticosSupervisionados.stream().map(Farmaceutico::getNome).collect(java.util.stream.Collectors.toList()) +
+            (gerenteDeEstoqueSupervisionado != null ? ", gerenteDeEstoqueSupervisionado=" + gerenteDeEstoqueSupervisionado.getNome() : "") +
+            '}';
+    }
+
+
+    public void adicionarOperadorSupervisionado(OperadorCaixa operador1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarOperadorSupervisionado'");
+    }
+
 }

3b5b042e1efe1c0bbb97ed19dcaaf305a3206aff
Rhuan471 <rhuanjosevoltolini@gmail.com>
2025-05-06 13:44:58 -0300

    feat

--- projrhuan/src/main/java/br/univille/projrhuan/repository/AdministradorRepository.java
+++ projrhuan/src/main/java/br/univille/projrhuan/repository/AdministradorRepository.java
@@ -7,5 +7,10 @@ import br.univille.projrhuan.entity.Administrador;
 
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador,Long>{
+    // Optional<Administrador> findById(Long id);
+    // List<Administrador> findAll();
+    // <S extends Administrador> S save(S entity);
+    // void deleteById(Long id);
+    // void update(Administrador administrador);
 
 }

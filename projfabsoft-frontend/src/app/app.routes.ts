dc3369ec8dce98cfe92a0cea3e12c8ad588f8bd8
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-05-27 23:41:39 +0000

    projfabsoftb-rhu

--- projfabsoft-frontend/src/app/app.routes.ts
+++ projfabsoft-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ClienteComponent } from './cliente/cliente.component';
+import { Cliente } from './model/cliente';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'clientes', component: ClienteComponent}
+];

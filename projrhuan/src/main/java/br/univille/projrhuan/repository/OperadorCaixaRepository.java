e001e9416f26a57dd44497d233477dca90512ae7
RHUAN JOSE VOLTOLINI <rhuanvoltolini@univille.br>
2025-06-03 18:58:07 -0300

    projrhuan

--- projrhuan/src/main/java/br/univille/projrhuan/repository/OperadorCaixaRepository.java
+++ projrhuan/src/main/java/br/univille/projrhuan/repository/OperadorCaixaRepository.java
@@ -1,4 +1,6 @@
 package br.univille.projrhuan.repository;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -6,6 +8,13 @@ import org.springframework.stereotype.Repository;
 import br.univille.projrhuan.entity.OperadorCaixa;
 
 @Repository
-public interface OperadorCaixaRepository extends JpaRepository<OperadorCaixa,Long>{
-
+public interface OperadorCaixaRepository extends JpaRepository<OperadorCaixa, Long> {
+    
+    public interface OperadorCaixaRepositorio {
+        Optional<OperadorCaixa> buscarPorId(Long id);
+        List<OperadorCaixa> listarTodos();
+        void salvar(OperadorCaixa OperadorCaixa);
+        void atualizar(OperadorCaixa OperadorCaixa);
+        void deletar(Long id);
+    }
 }

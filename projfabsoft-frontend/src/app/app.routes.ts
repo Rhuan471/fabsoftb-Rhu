3fa597d7f14d04df8be935452a6ff278f253f3ab
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-07-01 13:20:55 +0000

    fabsoft-Rhu

--- projfabsoft-frontend/src/app/app.routes.ts
+++ projfabsoft-frontend/src/app/app.routes.ts
@@ -1,17 +1,8 @@
 import { Routes } from '@angular/router';
-
-import { AdministradorComponent } from './administrador/administrador.component';
 import { ClienteComponent } from './cliente/cliente.component';
-import { FarmaceuticoComponent } from './farmaceutico/farmaceutico.component';
-import { FormAdministradorComponent } from './form-administrador/form-administrador.component';
 import { FormClienteComponent } from './form-cliente/form-cliente.component';
-import { FormFarmaceuticoComponent } from './form-farmaceutico/form-farmaceutico.component';
-import { FormGerenteEstoqueComponent } from './form-gerente-estoque/form-gerente-estoque.component';
-import { FormOperadorCaixaComponent } from './form-operador-caixa/form-operador-caixa.component';
-import { GerenteEstoqueComponent } from './gerente-estoque/gerente-estoque.component';
-import { OperadorCaixaComponent } from './operador-caixa/operador-caixa.component';
-import { MedicamentoListComponent } from './medicamento/medicamento.component';
 import { MedicamentoFormComponent } from './form-medicamento/form-medicamento.component';
+import { MedicamentoListComponent } from './medicamento/medicamento.component';
 
 export const routes: Routes = [
   // Redirect padrão para clientes
@@ -22,30 +13,10 @@ export const routes: Routes = [
   { path: 'clientes/novo', component: FormClienteComponent },
   { path: 'clientes/alterar/:id', component: FormClienteComponent },
 
-  // Rotas administradores
-  { path: 'administradores', component: AdministradorComponent },
-  { path: 'administradores/novo', component: FormAdministradorComponent },
-  { path: 'administradores/alterar/:id', component: FormAdministradorComponent },
-
   // Rotas medicamentos
   { path: 'medicamentos', component: MedicamentoListComponent },
   { path: 'medicamentos/novo', component: MedicamentoFormComponent },
   { path: 'medicamentos/alterar/:id', component: MedicamentoFormComponent },
-
-  // Rotas farmaceuticos
-  { path: 'farmaceuticos', component: FarmaceuticoComponent },
-  { path: 'farmaceuticos/novo', component: FormFarmaceuticoComponent },
-  { path: 'farmaceuticos/alterar/:id', component: FormFarmaceuticoComponent },
-
-  // Rotas operadores de caixa
-  { path: 'operadores-caixa', component: OperadorCaixaComponent },
-  { path: 'operadores-caixa/novo', component: FormOperadorCaixaComponent },
-  { path: 'operadores-caixa/alterar/:id', component: FormOperadorCaixaComponent },
-
-  // Rotas gerentes de estoque
-  { path: 'gerentes-estoque', component: GerenteEstoqueComponent },
-  { path: 'gerentes-estoque/novo', component: FormGerenteEstoqueComponent },
-  { path: 'gerentes-estoque/alterar/:id', component: FormGerenteEstoqueComponent },
 
   // Caso nenhuma rota bata
   { path: '**', redirectTo: 'clientes' }
--- projrhuan/src/main/java/br/univille/projrhuan/entity/Cliente.java
+++ projrhuan/src/main/java/br/univille/projrhuan/entity/Cliente.java
@@ -4,12 +4,10 @@ import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
@@ -28,12 +26,6 @@ public class Cliente {
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dataNascimento;
-
-    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH})
-    private Farmaceutico farmaceuticoResponsavel;
-
-    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH})
-    private OperadorCaixa operadorDeCaixaResponsavel;
 
     // Default constructor
     public Cliente() {
@@ -93,32 +85,5 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
-    }
-
-    public Farmaceutico getFarmaceuticoResponsavel() {
-        return farmaceuticoResponsavel;
-    }
-
-    public void setFarmaceuticoResponsavel(Farmaceutico farmaceuticoResponsavel) {
-        this.farmaceuticoResponsavel = farmaceuticoResponsavel;
-    }
-
-    public OperadorCaixa getOperadorDeCaixaResponsavel() {
-        return operadorDeCaixaResponsavel;
-    }
-
-    public void setOperadorDeCaixaResponsavel(OperadorCaixa operadorDeCaixaResponsavel) {
-        this.operadorDeCaixaResponsavel = operadorDeCaixaResponsavel;
-    }
-
-    @Override
-    public String toString() {
-        return "Cliente{" +
-            "nome='" + nome + '\'' +
-            ", cpf='" + cpf + '\'' +
-            ", endereco='" + endereco + '\'' +
-            (farmaceuticoResponsavel != null ? ", farmaceuticoResponsavel=" + farmaceuticoResponsavel.getNome() : "") +
-            (operadorDeCaixaResponsavel != null ? ", operadorDeCaixaResponsavel=" + operadorDeCaixaResponsavel.getNome() : "") +
-            '}';
     }
 }
